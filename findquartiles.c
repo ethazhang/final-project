9f94ecd54f4b025b93862027430ed12490913df3
ethazhang <ethan.z.abalos@gmail.com>
2026-03-28 22:33:18 -0400

    Revise comments for quartile calculation function
    Updated function documentation to reflect quartile calculation process.

---
+++ findquartiles.c
@@ -0,0 +1,15 @@
+/*
+a function to calculate the quartiles. In this function, (1) call the function bubbleSort(double
+arr[], int size) to sort the array; (2) call the function findMedian(double arr[], int start, int end) to
+find the median of the sorted array as Q2; (3) call the function findMedian(double arr[], int start, int
+end) to find the median of the lower half of the sorted array as Q1 and the median of the upper half
+as Q3.
+*/
+#include <stdio.h>
+
+void findQuartiles(double arr[], int size, double *Q1, double *Q2, double *Q3);
+
+int main(void)
+{
+  return 0;
+}
