693caf5f879716e62f16daefd6b323e8ed6e88ec
ethazhang <ethan.z.abalos@gmail.com>
2026-03-28 22:29:20 -0400

    Implement Bubble Sort algorithm in bubblesort.c
    Added a function to sort an array using Bubble Sort.

---
+++ bubblesort.c
@@ -0,0 +1,20 @@
+/*
+a function to sort an array using Bubble Sort. Bubble Sort is a simple and beginner-
+friendly sorting algorithm that repeatedly compares and swaps adjacent elements until the entire
+list is sorted.
+How Bubble Sort Works (Step-by-Step)
+1. Start from the first element and compare it with the next element.
+2. If the first element is greater, swap them.
+3. Move to the next pair and repeat the process.
+4. Continue this until the largest element moves to the end.
+5. Repeat the process for the remaining unsorted portion of the list.
+6. Continue until no swaps are needed.
+*/
+#include <stdio.h>
+
+void bubbleSort(double arr[], int size);
+
+int main(void)
+{
+  return 0;
+}
