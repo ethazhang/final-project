0a5508cf29f2129444e4db36c96a82a5b2b7ff80
ethazhang <ethan.z.abalos@gmail.com>
2026-03-28 22:52:24 -0400

    Create mean.c

---
+++ mean.c
@@ -0,0 +1,13 @@
+/*
+a function which computes the average of all the values in the array arr[]. The formula for the average is Σarr[i] /
+size of arr[]. For example, the mean of the above 30 values (not counting the first value which means
+day 1) is 2.97.
+*/
+#include <stdio.h>
+
+double mean(double arr[], int size);
+
+int main(void)
+{
+  return 0;
+}
