d5310cf9c428e61d6a43d6e0f2fd9e8560ba1583
ethazhang <ethan.z.abalos@gmail.com>
2026-04-01 21:03:25 -0400

    completed swap.c

--- swap.c
+++ swap.c
@@ -3,10 +3,19 @@
 
 void swap(double *a, double *b)
 {
-  
+  double temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 int main(void)
 {
+  /* testing function */
+  double x = 1.0, y = 2.0;
+  printf("Before swap: x = %lf, y = %lf\n", x, y);
+  swap(&x, &y);
+  printf("After swap: x = %lf, y = %lf\n", x, y);
+
   return 0;
 }
